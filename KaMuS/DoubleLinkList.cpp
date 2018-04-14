aa6da78fecd078c16bb793005b6f505455f6ef93
alvinsyahfril <alvinsyahfrilnurfaiz@gmail.com>
2018-04-14 16:08:36 +0700

    nambahin Double
    after, deklarasi di struktur data belum

--- KaMuS/DoubleLinkList.cpp
+++ KaMuS/DoubleLinkList.cpp
@@ -1,1 +1,102 @@
 #include "DoubleLinkList.h"
+
+
+void createList(linklist &L){
+    first(L) = NULL;
+    last(L) = NULL;
+}
+
+address alokasi(){
+    address P;
+    P = new elemen;
+    next(P) = NULL;
+    prev(P) = NULL;
+
+    return P;
+}
+
+insertFirst(linklist &L, address data){
+    if (first(L) == NULL){
+        first(L) = data;
+        last(L) = data;
+    }else{
+        next(data) = first(L);
+        prev(first(L)) = data;
+        first(L) = data;
+    }
+}
+
+insertLast(linklist &L, address data){
+    if (first(L) == NULL){
+        first(L) = data;
+        last(L) = data;
+    }else{
+        prev(data) = last(L);
+        next(last(L)) = data;
+        last(L) = data;
+    }
+}
+
+void show(linklist L){
+    if (first(L) == NULL){
+        cout<< "DATA KOSONG";
+    }else{
+        address P;
+        P = first(L);
+        while( P != NULL){
+            cout<< "blabla";
+            P = next(P);
+        }
+    }
+}
+
+void deleteLast(linklist &L){
+    if(first(L) == NULL){
+        cout<< "DATA KOSONG";
+    }else if(first(L) == last(L)){
+        address P;
+        P = first(L);
+        first(L) = NULL;
+        last(L) = NULL;
+        delete P;
+    }else{
+        address P;
+        P = last(L);
+        last(L) = prev(P);
+        next(last(P)) = NULL;
+        prev(P) = NULL;
+        delete P;
+    }
+}
+
+void deleteFirst(linklist &L){
+    if(first(L) == NULL){
+        cout << "DATA KOSONG";
+    }else{
+        address P;
+        P = first(L);
+        first(L) = NULL;
+        last(L) = NULL;
+        delete P;
+    }else {
+        address P;
+        P = first(L);
+        first(L) = next(P);
+        (prev(first(L)) = NULL;
+        next(P) = NULL;
+        delete P;
+    }
+}
+
+void cari(linklist L, address data);
+    if(first(L) == NULL){
+        return NULL;
+    }else{}
+
+
+
+
+
+
+
+
--- KaMuS/DoubleLinkList.h
+++ KaMuS/DoubleLinkList.h
@@ -1,6 +1,14 @@
 #ifndef DOUBLELINKLIST_H_INCLUDED
 #define DOUBLELINKLIST_H_INCLUDED
 
+#include "mainHeader.h"
+#define first(L) L.first
+#define info(P) P->info
+#define next(P) P->next
+
+using namespace std;
+
+
 
 
 #endif // DOUBLELINKLIST_H_INCLUDED
