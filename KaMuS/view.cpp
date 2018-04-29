9bde12c3c6a4242684a0d4438da4263c619c6214
MAqielHilmanM <maqiel2600@gmail.com>
2018-04-29 20:41:58 +0700

    alvin benerin

---
+++ KaMuS/view.cpp
@@ -0,0 +1,27 @@
+#include "view.h"
+
+void mainView(){
+    //Variable
+    string huruf;
+
+
+    cout<<"================================="<<endl;
+    cout<<"        APLIKASI KAMUS         "<<endl;
+    cout<<"================================="<<endl;
+    ShowCurrentTime();
+    cout<<endl;
+    cout<<"================================="<<endl;
+    cout<<"Silahkan Pilih Menu Utama"<<endl;
+    cout<<"1. Inggris to Indonesia"<<endl;
+    cout<<"2. Indonesia to Inggris"<<endl;
+    int pilih;
+    cout<<"Masukan Pilihan : ",cin>>pilih;
+    if (pilih == 1){
+    }else if (pilih == 2){
+        cout<<"Masukan kata menggunakan huruf KAPITAL : ",cin>>huruf;
+    }
+}
+
+void InputView(int menu){
+    cout<<"Masukan kata menggunakan huruf KAPITAL : ",cin>>huruf;
+}
--- KaMuS/view.h
+++ KaMuS/view.h
@@ -1,6 +1,7 @@
 #ifndef VIEW_H_INCLUDED
 #define VIEW_H_INCLUDED
 
+#include "mainHeader.h"
 
 
 #endif // VIEW_H_INCLUDED
